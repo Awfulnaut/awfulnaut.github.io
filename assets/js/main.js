2e7944a99d4e9bf61860a8ea8067d358eaded504
Cadin McQueen <cadin.mcqueen@gmail.com>
2018-10-01 14:17:51 -0400

    Smooth scrolling anchors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,49 @@
 "use strict";
 
+function scrollTo(hash) {
+  location.hash = "#" + hash;
+}
+
 $(document).ready(function () {
 
-  // Sidebar nav behavior
+  // SIDEBAR BEHAVIOR
   $('.menu__button').on('click', function () {
     $('#sidebar, #content').toggleClass('active');
     $('.collapse.in').toggleClass('in');
     $('a[aria-expanded=true]').attr('aria-expanded', 'false');
   });
 
+  // SCROLL BEHAVIOR
+  // Select all links with hashes
+  $('[data-toggle="elementscroll"]').click(function (event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 700, function () {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 });
