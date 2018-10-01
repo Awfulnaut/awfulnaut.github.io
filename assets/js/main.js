5618f64484fa4ffc0b08831089810c7c6e67c94f
Cadin McQueen <cadin.mcqueen@gmail.com>
2018-10-01 00:33:03 -0400

    Add collapsable sidebar nav

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+$(document).ready(function () {
+
+  // Sidebar nav behavior
+
+  $("#sidebar").mCustomScrollbar({
+    theme: "minimal"
+  });
+
+  $('#sidebarCollapse').on('click', function () {
+    $('#sidebar, #content').toggleClass('active');
+    $('.collapse.in').toggleClass('in');
+    $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+  });
+
+});
